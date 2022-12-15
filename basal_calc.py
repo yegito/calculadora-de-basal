78a75488444b26720c5d754eb8ea4bf30685fb0d
yegito <yuriegito@hotmail.com>
2022-12-15 18:08:34 -0300

    ap IMC

--- basal_calc.py
+++ basal_calc.py
@@ -1,4 +1,26 @@
-def calcularora_basal():
+def imc_or_basal():
+    escolha = input("Você quer calcular IMC ou BASAL?")
+    
+    if escolha.upper == "IMC" or "imc":
+        calcular_imc()
+    else:
+        calculadora_basal()
+      
+        
+def calcular_imc():
+    name = input("Olá meu nobre internalta, qual o seu nome? ")
+    print(f"Então vamos lá {name}! ")
+    peso = int(input(f"{name} qual o seu peso? "))
+    altura = int(input(f"Já temos o seu peso {name}, agora só falta sua altura. qual a sua altura? "))
+    
+    altura_imc = altura * altura
+    
+    imc = peso / altura_imc
+    
+    print(f"Seu IMC é {imc}")
+    
+
+def calculadora_basal():
     name = input("Olá meu nobre internalta, qual o seu nome? ")
     print(f"Então vamos lá {name}! ")
     idade = int(input(f"Qual a sua idade {name}? "))
@@ -14,12 +36,13 @@ def calcularora_basal():
     print(f"Seu basal é {basal}")
     print(f"Seu corpo consome {basal} calorias diariamente só para se manter")
     
+    
 def recalcular():
     while True:
         print("Você quer calcular seu Basal novamente? ")
         recal = input("Aperte Y para sim e N para não! ")
         if recal.upper() == "Y":
-             calcularora_basal()
+             calculadora_basal()
         else:
              print("até mais!")
              break
@@ -27,5 +50,4 @@ def recalcular():
     
     
     
-calcularora_basal()
-recalcular()
+imc_or_basal()
